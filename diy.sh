6e359e17197a68be19e1f736ed7efa92cc2f3401
KleinerSource <KleinerSource@me.com>
2020-02-27 12:08:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,8 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 增加软件包
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/AdguardTeam/AdGuardHome.git package/AdGuardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
