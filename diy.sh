2b6443c3134e936f155d3d7dff5c2c8db2b24059
KleinerSource <KleinerSource@me.com>
2020-02-27 12:09:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
 
 # 增加软件包
 git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
