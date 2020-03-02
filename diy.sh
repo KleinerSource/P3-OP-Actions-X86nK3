17ea467539336d1ac25aeff5f6bbf42a6ea5008e
KleinerSource <KleinerSource@me.com>
2020-03-02 14:12:14 +0800

    update
    add kpr
    remove samba

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题为Argon
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
 # git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/kleinersource/luci-theme-argon_new
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 
@@ -32,11 +32,10 @@ git clone https://github.com/destan19/OpenAppFilter.git package/kleinersource/Op
 # git clone https://github.com/RealKiro/Luci-SmartDNS.git package/kleinersource/Luci-SmartDNS
 
 # KPR plus+
-# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/kleinersource/luci-app-koolproxyR
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/kleinersource/luci-app-koolproxyR
 
 # 魔改 SSRP+
-# git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo.git package/kleinersource/luci-app-ssr-plus-Jo
-git clone https://github.com/Sweet-Fairy/luci-app-ssr-plus-Jo package/kleinersource/luci-app-ssr-plus-Jo
+git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo.git package/kleinersource/luci-app-ssr-plus-Jo
 git clone https://github.com/Leo-Jo-My/my.git package/my
 rm -rf package/my/openwrt-dnsforwarder
 rm -rf package/my/openwrt-simple-obfs
