d913c83ee5bab472e420308f4b041f3728cafd83
KleinerSource <KleinerSource@me.com>
2020-03-02 16:14:35 +0800

    Update

--- diy.sh
+++ diy.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
+# 说明: DIY 脚本
 # Lisence: MIT
-# Author: P3TERX
+# 作者: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
+# 修改默认 IP
 sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
 
-# 更改默认主题为Argon
+# 更改默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-# git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/kleinersource/luci-theme-argon_new
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 
-# 复杂的AdGuardHome的openwrt的luci界面
+# AdGuardHome 的 OpenWrt Luci
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/kleinersource/luci-app-adguardhome
 
-# 网易云音乐
+# 网易云音乐完整版
 git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/kleinersource/luci-app-unblockneteasemusic
 
 # OpenClash
@@ -37,9 +35,13 @@ git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/kle
 # 魔改 SSRP+
 git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo.git package/kleinersource/luci-app-ssr-plus-Jo
 git clone https://github.com/Leo-Jo-My/my.git package/my
-rm -rf package/my/openwrt-dnsforwarder
-rm -rf package/my/openwrt-simple-obfs
-rm -rf package/my/openwrt-v2ray-plugin
+rm -rf package/lean/dnsforwarder
+rm -rf package/lean/simple-obfs
+rm -rf package/lean/v2ray-plugin
+# rm -rf package/my/openwrt-dnsforwarder
+# rm -rf package/my/openwrt-simple-obfs
+# rm -rf package/my/openwrt-v2ray-plugin
 
+# 刷新&安装源
 ./scripts/feeds update -a
 ./scripts/feeds install -a
