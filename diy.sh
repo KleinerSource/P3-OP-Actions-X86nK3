0c0dbeaa47492de6dcd383dd42e9dc0c1e1a0898
KleinerSource <KleinerSource@me.com>
2020-04-08 19:37:41 +0800

    F***XQ

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # 修改默认 IP
-sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题为 Argon
 rm -rf package/lean/luci-theme-argon  
