98fc8170c5f74792ea82357285ad92419190d952
KleinerSource <KleinerSource@me.com>
2021-03-08 11:09:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -40,11 +40,7 @@ git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/kle
 # git clone  https://github.com/lisaac/luci-app-dockerman.git package/kleinersource/luci-app-dockerman        
 
 # SSRP
-#git clone -b master https://github.com/fw876/helloworld ./tmp/helloworld
-#cp -rf ./tmp/helloworld/luci-app-ssr-plus ./package/lean/
-#cp -rf ./tmp/helloworld/tcping ./package/lean/
-#cp -rf ./tmp/helloworld/naiveproxy ./package/lean/
-#rm -rf ./tmp/
+git clone -b master https://github.com/fw876/helloworld package/ssrplus
 
 # 刷新&安装源
 ./scripts/feeds update -a
