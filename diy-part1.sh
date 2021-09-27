db65adfe7b94ef15ea67e112f77cd598fd3f77de
KleinerSource <KleinerSource@me.com>
2021-09-27 14:07:00 +0800

    up

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #=================================================
-# 说明: DIY 脚本
-# Lisence: MIT
 # 文件名: diy-part1.sh
 # 描述: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
 # 作者: P3TERX
 # Blog: https://p3terx.com
 #=================================================
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Add a feed source
 # Xiaorouji Passwall
 # sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #=================================================
-# 说明: DIY 脚本
+# 文件名: diy-part2.sh
+# 描述: OpenWrt DIY script part 2 (After Update feeds)
 # Lisence: MIT
 # 作者: P3TERX
 # Blog: https://p3terx.com
 #=================================================
 # 修改默认 IP
 # sed -i 's/192.168.1.1/10.10.10.111/g' package/base-files/files/bin/config_generate
+
+# 修改编译信息
 sed -i 's/%D %V, %C/%D %V, %C, Build by KleinerSource/g' package/base-files/files/etc/banner
 
 # 更改默认主题为 Argon
