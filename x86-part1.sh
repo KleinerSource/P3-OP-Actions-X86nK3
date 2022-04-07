98391c17aebfa803557a7c246353d59131e0228a
KleinerSource <KleinerSource@me.com>
2022-04-07 17:08:20 +0800

    Test

--- x86-part1.sh
+++ x86-part1.sh
@@ -9,7 +9,43 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# 添加 feed 源
 # Xiaorouji Passwall
 sed -i '$a src-git xiaorouji https://github.com/WYC-2020/openwrt-passwall.git' feeds.conf.default
+
+# 替换原有 Argon 主题
+rm -rf package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+
+# Rosy 主题
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/kleinersource/luci-theme-rosy
+
+# AdGuardHome 的 OpenWrt Luci
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/kleinersource/luci-app-adguardhome
+
+# 网易云音乐完整版
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/kleinersource/luci-app-unblockneteasemusic
+
+# 管控上网行为
+# git clone https://github.com/destan19/OpenAppFilter.git package/kleinersource/OpenAppFilter
+
+# SmartDNS
+# git clone https://github.com/RealKiro/Luci-SmartDNS.git package/kleinersource/Luci-SmartDNS
+
+# KPR plus+
+# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/kleinersource/luci-app-koolproxyR
+
+# OpenClash
+# git clone https://github.com/vernesong/OpenClash.git package/kleinersource/OpenClash
+
+# PassWall
+git clone https://github.com/WYC-2020/openwrt-passwall.git  package/kleinersource/passwall
+
+# SSRP
+git clone -b master https://github.com/fw876/helloworld package/ssrplus
+
+# vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/kleinersource/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/kleinersource/luci-app-vssr
 
--- x86-part2.sh
+++ x86-part2.sh
@@ -13,44 +13,4 @@
 sed -i 's/%D %V, %C/%D %V, %C, Build by KleinerSource/g' package/base-files/files/etc/banner
 
 # 更改默认主题为 Argon
-rm -rf package/lean/luci-theme-argon  
-git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/kleinersource/luci-theme-argon
-
-# AdGuardHome 的 OpenWrt Luci
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/kleinersource/luci-app-adguardhome
-
-# 网易云音乐完整版
-git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/kleinersource/luci-app-unblockneteasemusic
-
-# 管控上网行为
-# git clone https://github.com/destan19/OpenAppFilter.git package/kleinersource/OpenAppFilter
-
-# Rosy 主题
-git clone https://github.com/rosywrt/luci-theme-rosy.git package/kleinersource/luci-theme-rosy
-
-# SmartDNS
-# git clone https://github.com/RealKiro/Luci-SmartDNS.git package/kleinersource/Luci-SmartDNS
-
-# KPR plus+
-# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/kleinersource/luci-app-koolproxyR
-
-# Docker
-# git clone  https://github.com/lisaac/luci-lib-docker.git package/kleinersource/luci-lib-docker
-# git clone  https://github.com/lisaac/luci-app-dockerman.git package/kleinersource/luci-app-dockerman        
-
-# OpenClash
-# git clone https://github.com/vernesong/OpenClash.git package/kleinersource/OpenClash
-
-# PassWall
-git clone https://github.com/WYC-2020/openwrt-passwall.git  package/kleinersource/passwall
-
-# SSRP
-git clone -b master https://github.com/fw876/helloworld package/ssrplus
-
-# vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/kleinersource/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/kleinersource/luci-app-vssr
-
-# 刷新&安装源
-./scripts/feeds update -a
-./scripts/feeds install -a
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
